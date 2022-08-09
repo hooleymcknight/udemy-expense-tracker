9a413f5dbd0e4f2feeecba535b494ace8520ad26
hooleymcknight <hmphillips93@gmail.com>
2022-08-08 21:17:59 -0700

    moved expenses list to its own component, up to date right before assignment 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
-import ExpenseItem from './ExpenseItem'
+import ExpensesList from './ExpensesList'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
+import './ExpensesList.css'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020')
@@ -12,19 +13,15 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear)
   }
 
-  const card_classes = `expenses ${filteredYear}`
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
 
   return (
     <div>
-      <Card className={card_classes}>
+      <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          shown={expense.date.getFullYear() == filteredYear}
-        />)}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
